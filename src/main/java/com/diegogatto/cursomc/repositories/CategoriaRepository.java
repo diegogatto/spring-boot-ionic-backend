70823199d70665d5b319e1d2a43b3d3463945260
Diego Gatto <diego.gatto@hotmail.com>
2019-02-17 18:44:09 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/diegogatto/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.diegogatto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.diegogatto.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
